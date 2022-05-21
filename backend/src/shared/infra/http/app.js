fe2825bc21c0aa633e6d019f1b38217cd77352e3
Spinnafre <davispenha@gmail.com>
2022-05-21 11:50:43 -0300

    Ajustes no package.json

--- backend/src/shared/infra/http/app.js
+++ backend/src/shared/infra/http/app.js
@@ -1,13 +1,11 @@
 const UFCNewsRouter=require('./routes/UFCNews.routes')
 const RURouter=require('./routes/RU.routes')
 
+// https://puppeteer.github.io/puppeteer/docs
 const app=require("express")()
-const puppeteer = require('puppeteer')
-const { removeBlanksLines } = require('../../../../utils/removeBlanksLines')
 
 app.use('/news',UFCNewsRouter)
 app.use('/ru_ufc',RURouter)
-// https://puppeteer.github.io/puppeteer/docs/puppeteer.elementhandle/
 
 module.exports={
     app
