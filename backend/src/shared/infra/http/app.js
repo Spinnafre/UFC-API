6e275295939e5808937aab1a1acdd41b45a28520
Spinnafre <davispenha@gmail.com>
2022-05-21 12:51:20 -0300

    Ajustes na documentação Swagger

--- backend/src/shared/infra/http/app.js
+++ backend/src/shared/infra/http/app.js
@@ -1,11 +1,14 @@
 const UFCNewsRouter=require('./routes/UFCNews.routes')
 const RURouter=require('./routes/RU.routes')
+const swaggerUi=require("swagger-ui-express")
+const swaggerFile=require("../../../swagger.json")
 
 // https://puppeteer.github.io/puppeteer/docs
 const app=require("express")()
 
 app.use('/news',UFCNewsRouter)
 app.use('/ru_ufc',RURouter)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports={
     app
