e5e0f44b070c44034bfff3643a7e70eccde2882a
Spinnafre <davispenha@gmail.com>
2023-09-03 17:22:28 -0300

    feat: add pino to logging

--- src/main/config/server.ts
+++ src/main/config/server.ts
@@ -1,3 +1,4 @@
 export default {
   port: process.env.PORT || 8080,
+  logsLevels: process.env.LOG_LEVEL || "info",
 };
--- src/shared/infra/logger/logger.ts
+++ src/shared/infra/logger/logger.ts
@@ -1,46 +1,47 @@
+import pino from "pino";
+import serverConfig from "../../../main/config/server";
 export class Logger {
-  private static loggerConfig = {
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-    hour: "numeric",
-    minute: "2-digit",
-    second: "numeric",
-  };
-  static warn(log: any) {
-    console.log(
-      `[WARNING] - ${Intl.DateTimeFormat("pt-BR", {
+  private static logger = pino({
+    level: serverConfig.logsLevels,
+    formatters: {
+      level: (label) => {
+        return { level: label.toUpperCase() };
+      },
+      bindings: (bindings) => {
+        return {
+          proccess_id: bindings.pid,
+          host: bindings.hostname,
+          node_version: process.version,
+        };
+      },
+    },
+    timestamp: () =>
+      `, "timestamp":"${Intl.DateTimeFormat("pt-BR", {
         day: "2-digit",
         month: "2-digit",
         year: "2-digit",
         hour: "numeric",
         minute: "2-digit",
         second: "numeric",
-      }).format(Date.now())} - ${log}`
-    );
+      }).format(Date.now())}"`,
+    transport: {
+      target: "pino-pretty",
+      options: {
+        levelFirst: true,
+        ignore: "node_version, host",
+        singleLine: true,
+        translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+        customPrettifiers: {},
+      },
+    },
+  });
+  static warn(log: any) {
+    this.logger.warn(log);
   }
   static info(log: any) {
-    console.log(
-      `[INFO] - ${Intl.DateTimeFormat("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit",
-        hour: "numeric",
-        minute: "2-digit",
-        second: "numeric",
-      }).format(Date.now())} - ${log}`
-    );
+    this.logger.info(log);
   }
   static error(log: any) {
-    console.log(
-      `[ERROR] - ${Intl.DateTimeFormat("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit",
-        hour: "numeric",
-        minute: "2-digit",
-        second: "numeric",
-      }).format(Date.now())} - ${log}`
-    );
+    this.logger.error(log);
   }
 }
