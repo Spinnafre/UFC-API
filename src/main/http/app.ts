8af3cc13f2049ca693bd6c29084924ae12248c0b
Spinnafre <davispenha@gmail.com>
2023-10-13 23:16:21 -0300

    build: add morgan and helmet

--- src/main/http/app.ts
+++ src/main/http/app.ts
@@ -2,6 +2,8 @@ import { handleError } from "../../shared/errors/handler";
 import express from "express";
 import { setupRoutes } from "./routes/setup-routes";
 import { Logger } from "../../shared/infra/logger/logger";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,13 @@ app.use(express.json());
 app.use(async (err: any, req: any, res: any, next: any) => {
   await handleError(err, res);
 });
+
+app.use(helmet());
+app.use(
+  morgan(
+    ":date[iso] :remote-addr :remote-user :method :url :status :res[content-length] - :response-time ms"
+  )
+);
 
 setupRoutes(app);
 // process.on("uncaughtException", (err) => {
